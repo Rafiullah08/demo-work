5959b009f51e16f7e6b7829bc18061d60368dbf9
Rafiullah <140521887+Rafiullah08@users.noreply.github.com>
2024-06-03 13:45:42 -0700

    add services page

---
+++ New folder/script.js
@@ -0,0 +1,9 @@
+function openOrderForm(service) {
+  var orderFormPopup = document.getElementById('order-form-popup');
+  orderFormPopup.style.display = 'block';
+}
+
+function closeOrderForm() {
+  var orderFormPopup = document.getElementById('order-form-popup');
+  orderFormPopup.style.display = 'none';
+}
